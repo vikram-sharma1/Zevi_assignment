9ce40ce7f651a01d350913eae6e11565313d6fe4
Vikram Sharma <thevikramdeveloper@gmail.com>
2022-08-14 13:33:08 +0530

    filter

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -12,11 +12,21 @@ const ProductPage = () => {
     const [price, setPrice] = useState(true)
     const [ratings, setRatings] = useState(true)
     const [data, setData] = useState([])
+
+
     const [wish, setWish] = useState(true)
+    const [id, setId] = useState()
+    const [array, setArray] = useState([])
+
+
+   
     
     useEffect(()=>{
         FetchData()
     },[])
+
+
+
 
     const changeStatus1 = () => {
         if(brand == true){
@@ -48,21 +58,157 @@ const ProductPage = () => {
         axios.get(`https://fake-ecommerce-server.herokuapp.com/products`).then((res)=>{
             // console.log(res.data)
             setData(res.data)
+            
         }).catch((err)=>{
              console.log('err:', err)
 
         })
     }
 
-    const colorChange = () => {
-        if(wish == true){
-            setWish(false)
+
+    const handleBrandChange = (e) => {
+        // console.log(e.target.checked, e.target.id)
+
+        if(e.target.id == 'mens'){
+            if(e.target.checked){
+                const mens_filter = data.filter((el) => el.category === "men's clothing")
+                setData([...mens_filter])
+            }
+            else{
+                FetchData()
+            }
         }
-        else{
-            setWish(true)
+        else if(e.target.id == 'womens'){
+            if(e.target.checked ){
+                const womens_filter = data.filter((el) => el.category === "women's clothing")
+                setData([...womens_filter])
+            }
+            else{
+                FetchData()
+            }
+        }
+        else if(e.target.id == 'bags'){
+            if(e.target.checked ){
+                const bags_filter = data.filter((el) => el.category === "bags")
+                setData([...bags_filter])
+            }
+            else{
+                FetchData()
+            }
+        }
+        else if(e.target.id == 'jewellery'){
+            if(e.target.checked ){
+                const jewellery_filter = data.filter((el) => el.category === "jewelery")
+                setData([...jewellery_filter])
+            }
+            else{
+                FetchData()
+            }
+        }
+        else if(e.target.id == 'electronics'){
+            if(e.target.checked ){
+                const electronics_filter = data.filter((el) => el.category === "electronics")
+                setData([...electronics_filter])
+            }
+            else{
+                FetchData()
+            }
         }
 
     }
+
+    const handlePriceChange = (e) => {
+    
+        if(e.target.id == 'under500'){
+            if(e.target.checked){
+                const under500 = data.filter((el) => el.price < 500)
+                setData([...under500])
+            }
+            else{
+                FetchData()
+            }
+        }
+        else if(e.target.id == 'above500'){
+            if(e.target.checked){
+                const above500 = data.filter((el) => el.price > 500)
+                setData([...above500])
+            }
+            else{
+                FetchData()
+            }
+        }
+
+
+    }
+
+    const handleRatingChange = (e) => {
+        console.log('e:', e)
+        if(e.target.id == 'five'){
+            console.log('five')
+            if(e.target.checked){
+                const five = data.filter((el) => el.rating.rate == 5 )
+                setData([...five])
+            }
+            else{
+                FetchData()
+            }
+        }
+        else if(e.target.id == 'four'){
+            if(e.target.checked){
+                const four = data.filter((el) => el.rating.rate == 4 )
+                setData([...four])
+            }
+            else{
+                FetchData()
+            }
+        }
+        else if(e.target.id == 'three'){
+            if(e.target.checked){
+                const three = data.filter((el) => el.rating.rate == 3 )
+                setData([...three])
+            }
+            else{
+                FetchData()
+            }
+        }
+        else if(e.target.id == 'two'){
+            if(e.target.checked){
+                const two = data.filter((el) => el.rating.rate == 2 )
+                setData([...two])
+            }
+            else{
+                FetchData()
+            }
+        }
+        else if(e.target.id == 'one'){
+            if(e.target.checked){
+                const one = data.filter((el) => el.rating.rate == 1 )
+                setData([...one])
+            }
+            else{
+                FetchData()
+            }
+        }
+    }
+
+    
+
+    const handleWishList = (i) => {
+
+        setArray([...array,id])
+
+            if(wish == false){
+                setWish(true)
+            }
+            else{
+                setWish(false)
+            }
+            setId(i)
+
+        
+    }
+
+    
 
   return (
     <>
@@ -77,19 +223,19 @@ const ProductPage = () => {
                             <h3><AiOutlineDown onClick={changeStatus1}/></h3>
                         </div>
                         <div className='Brandnames' style={{display : brand? "block" : "none"}}>
-                            <input type="checkbox" name="" id="" /> 
+                            <input type="checkbox" name="mens" id="mens" onChange={handleBrandChange} /> 
                             <label htmlFor="">Men's</label>
                                 <br />
-                            <input type="checkbox" name="" id="" /> 
+                            <input type="checkbox" name="womens" id="womens" onChange={handleBrandChange} /> 
                             <label htmlFor="">Women's</label>
                                 <br />
-                            <input type="checkbox" name="" id="" /> 
+                            <input type="checkbox" name="bags" id="bags" onChange={handleBrandChange} /> 
                             <label htmlFor="">Bags</label>
                                 <br />
-                            <input type="checkbox" name="" id="" /> 
+                            <input type="checkbox" name="jewellery" id="jewellery" onChange={handleBrandChange} /> 
                             <label htmlFor="">Jewellery</label>
                                 <br />
-                            <input type="checkbox" name="" id="" /> 
+                            <input type="checkbox" name="electronics" id="electronics" onChange={handleBrandChange} /> 
                             <label htmlFor="">Electronics</label>
                         </div>
                     </div>
@@ -99,10 +245,10 @@ const ProductPage = () => {
                             <h3><AiOutlineDown onClick={changeStatus2}/></h3>
                         </div>
                         <div className='Brandnames' style={{display : price? "block" : "none"}}>
-                            <input type="checkbox" name="" id="" /> 
+                            <input type="checkbox" name="under500" id="under500" onChange={handlePriceChange} /> 
                             <label htmlFor="">Under 500</label>
                                 <br />
-                            <input type="checkbox" name="" id="" /> 
+                            <input type="checkbox" name="above500" id="above500" onChange={handlePriceChange} /> 
                             <label htmlFor="">1000 To 3000</label>
                         </div>
                     </div>
@@ -112,20 +258,20 @@ const ProductPage = () => {
                             <h3><AiOutlineDown onClick={changeStatus3}/></h3>
                         </div>
                         <div className='Brandnames' style={{display : ratings? "block" : "none"}}>
-                            <input type="checkbox" name="" id="" /> 
-                            <label htmlFor=""><Rating name="read-only" value='4'  readOnly /></label>
+                            <input type="checkbox" name=""id='five' readOnly onChange={handleRatingChange}  /> 
+                            <label htmlFor=""><Rating name="read-only" value='5'/></label>
                                 <br />
-                            <input type="checkbox" name="" id="" /> 
-                            <label htmlFor=""><Rating name="read-only" value='4' readOnly /></label>
+                            <input type="checkbox" name="" id='four'  readOnly onChange={handleRatingChange} /> 
+                            <label htmlFor=""><Rating name="read-only" value='4'/></label>
                                 <br />
-                            <input type="checkbox" name="" id="" /> 
-                            <label htmlFor=""><Rating name="read-only" value='3' readOnly /></label>
+                            <input type="checkbox" name="" id='three' readOnly onChange={handleRatingChange} /> 
+                            <label htmlFor=""><Rating name="read-only" value='3'  /></label>
                                 <br />
-                            <input type="checkbox" name="" id="" /> 
-                            <label htmlFor=""><Rating name="read-only" value='2' readOnly /></label>
+                            <input type="checkbox" name="" id='two' readOnly onChange={handleRatingChange} /> 
+                            <label htmlFor=""><Rating name="read-only" value='2'  /></label>
                                 <br />
-                            <input type="checkbox" name="" id="" /> 
-                            <label htmlFor=""><Rating name="read-only" value='1' readOnly /></label>
+                            <input type="checkbox" name="" id='one' readOnly onChange={handleRatingChange} /> 
+                            <label htmlFor=""><Rating name="read-only" value='1'  /></label>
                         </div>
                     </div>
                 </div>
@@ -133,12 +279,14 @@ const ProductPage = () => {
                     {data.map((el)=>{
                         return(
                             <div key={el.id}>
-                                <AiTwotoneHeart id='wishlistLogo'/>
-                                <AiTwotoneHeart className='wishlistLogored'/>
+                                <AiTwotoneHeart className={wish && array.includes(el.id) ? `wishlistLogo red` : `wishlistLogo`}  onClick={()=>handleWishList(el.id)}/>
                                 <img src={el.image} alt="" className='productImg' />
+                                <p className='view'>View Product</p>
+                                <div className='postion'>
                                 <h4>{el.title}</h4>
                                 <p>Rs. {el.price}</p>
                                 <p><Rating name="half-rating-read" defaultValue={el.rating.rate} precision={0.5} readOnly /> ({el.rating.count})</p>
+                                </div>
                             </div>
                         )
                     })}
